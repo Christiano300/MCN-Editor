66eae132386c5f4a2372a028b32c5cceb30e00d2
Christiano300 <christian.patzl@gmx.net>
2025-07-30 21:26:13 +0200

    move to /editor

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -6,6 +6,15 @@ export default defineConfig({
   build: {
     target: "esnext",
     minify: "esbuild",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vscode: ["vscode"],
+          languageclient: ["monaco-languageclient", "vscode-languageclient"],
+        },
+      },
+    },
   },
   resolve: { dedupe: ["vscode"] },
+  base: "/editor",
 });
